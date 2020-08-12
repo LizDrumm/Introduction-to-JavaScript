8cd41b0db6c60a1c76b9bf7a01eb610008420ada
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-08-11 21:21:04 -0500

    more edits

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-let votingAge= '19';
-if (votingAge >'18')
-{
-    console.log ('true');
+/*let votingAge= '19';
+if (votingAge >'18'){
+    return true;
 }
+console.log (votingAge);*/
+
+let votingAge= '19';
+if (votingAge >'18'){
+    console.log (true);
+}
+
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
 let color = "blue"
 let opinion = "harsh";
 if(opinion === "soft"){
-    color = "pink";
+    color = "blue";
 }
 else if (opinion === "harsh"){
     color = "red";
@@ -86,7 +92,7 @@ else if (age <.583 && age >=.333){
 }
 
 else if (age <.033 && age >=.166){
-return .1* weight;
+    return .1* weight;
 }
 }
 console.log (pounds(1,15));
@@ -104,54 +110,66 @@ console.log (pounds(1,15));
 let paper = 2
 let scissors =3 */
 
+
  /*function game (player){  
-    let computer= math.random()
+    let computer= math.floor(math.random()*4)
 
       if (player === 1 && computer ===1){
        return "tie";
       }
       else if (player ===1 && computer ===2) {
-        return " computer wins ";
+        return " computer choose paper! comptuer wins ";
            }
       else if (player ===1 && computer ===3) {
-            return "player wins ";
+            return "computer choose scissors! player wins ";
         }
         else if (player ===2 && computer ===1) {
-                return "player wins ";
+                return "computer choose rock! player wins ";
         }
         else if (player ===2 && computer ===2) {
             return "tie";
         }
         else if (player ===2 && computer ===3) {
-            return "computer wins ";
+            return "computer choose scissor! computer wins ";
         }
         else if (player ===3 && computer ===1) {
-            return "computer wins ";
+            return "computer choose rock! computer wins ";
         }
         else if (player ===3 && computer ===2) {
-            return "player wins ";
+            return "computer choose paper! player wins ";
         }
         else if (player ===3 && computer ===3) {
             return "tie";
     }
 }
-console.log ((1,));
+console.log (game (computer)); */
 
-
-
-
-
+let computerChoice = Math.random();
+if(computerChoice < 0.33){
+computerChoice = "rock";
+}else if(computerChoice >= 0.34 && computerChoice < 0.66){
+ computerChoice = "paper";
+}else if(computerChoice >= 0.66){
+  computerChoice = "scissors";
+}
+console.log("Computer chooses " + computerChoice);
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
-
+let kilometers = prompt("Enter distance in kilometers: ")
+let conversion = 0.621371
+let miles = kilometers * conversion
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
 
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+let feet = prompt("Enter distance in feet: ")
+let conversionrate = 30.48
+let CM = feet * conversionrate
+console.log(`${feet} feet is equal to ${CM} CM.`);
 
 
 
@@ -160,12 +178,13 @@ console.log ((1,));
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
-function annoyingSong(){
-for (let i=99-1; i>0; i--) {
-   
-}
-console.log (i);
+
+
+
+    for(let i=99-1; i>0; i--) {
+        let number =i
+    }
+    console.log (` ${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around (i) bottles of soda on the wall`); 
 
 
 /************************************************************** Task 7 **************************************************************/
@@ -177,9 +196,56 @@ console.log (i);
 //60s should be D 
 //and anything below 60 should be F
   
+/*const grades = [ 90, 80, 70, 60]
+for (let i=0; i < grades.length; i++) {
+        Avgmarks += students[i][1];
+        let avg = (Avgmarks/students.length);{}
 
-  
-  
+        if (avg < 60){
+          console.log("Grade : F");      
+          } 
+        else if (avg < 70) {
+                console.log("Grade : D"); 
+                  } 
+        else if (avg < 80) 
+             {
+                console.log("Grade : C"); 
+        } else if (avg < 90) {
+                console.log("Grade : B"); 
+        } else if (avg < 100) {
+                console.log("Grade : A"); 
+}
+console.log((Avgmarks)/students.length);
+}
+
+function pounds(age, weight){
+
+if (age>=1 && weight<=5){
+        return .05*weight;
+}
+else if (age>=1 && weight>5 && weight<=10){
+   return .04*weight;
+}
+else if (age>=1 && weight>11 && weight<=15){
+    return .03*weight;
+}
+else if (age>=1 && wight>15) {
+    return .02*weight;
+}
+else if (age<1 && age >= .583) {
+    return .04 *weight;
+}
+else if (age <.583 && age >=.333){
+    return .05*weight;
+}
+
+else if (age <.033 && age >=.166){
+    return .1* weight;
+}
+}
+console.log (pounds(1,15));
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
